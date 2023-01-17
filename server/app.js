c7dfc956f6f8825dba6cbf203e66829460a85890
Mohakim231 <mohamedhakim0381@gmail.com>
2023-01-17 10:52:39 +0000

    Adding goats

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+const { goats, nextId } = require("./goats");
+
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Everyone");
+    res.json({
+        "message": "Hello, World"
+    });
+})
+
+app.get("/goats", (req, res) => {
+    res.json(goats);
 })
 
 module.exports = app;
---
+++ server/goats.js
@@ -0,0 +1,24 @@
+const goats = [
+        {   "id" : 1,   
+            "name": "Billy",
+            "age": 6,
+            "sex": "M",
+            "favouriteColour": "magenta"
+        },
+        {   "id" : 2,
+            "name": "John",
+            "age": 8,
+            "sex": "M",
+            "favouriteColour": "purple"
+        },
+        {   "id" : 3,
+            "name": "Beehronica",
+            "age": 2,
+            "sex": "M",
+            "favouriteColour": "grey"
+        }
+];
+
+let nextId = 4;
+
+module.exports = {goats, nextId};
